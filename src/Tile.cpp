ffbbb746f418665a1e6934f5fb8d1516bc31fed9
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-07 17:15:47 +0000

    Implemented the copyData method in the Tile class

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -18,3 +18,11 @@ unsigned char Tile::getPixel(int x, int y) const{
 void Tile::setPixel(int x, int y, unsigned char val){
     data[y * wd + x] = val; //set pixel data
 }
+
+void Tile::copyData(const unsigned char* source, int sourceWd, int sourceHt){
+    for (int y = 0; y < ht; ++y){
+        for (int x = 0; x < wd; ++x){
+            setPixel(x, y, source[y + sourceWd + x]); //copy pixel data
+        }
+    }
+}
