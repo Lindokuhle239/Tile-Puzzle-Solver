7c37102067794506d0c3e081a2ad0a1b3e909b52
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-07 18:14:19 +0000

    Added the generateRandMove and writeBoardState methods in the TileManager header

--- src/TileManager.h
+++ src/TileManager.h
@@ -8,4 +8,7 @@ class TileManager{
     public:
         TileManager(int size, const std::string& image); //constructor
         ~TileManager(); //destructor
+
+        void generateRandMove(); //generate a random valid move
+        void writeBoardState(const std::string& fileName) const; //write the board state to a pgm file
 }
