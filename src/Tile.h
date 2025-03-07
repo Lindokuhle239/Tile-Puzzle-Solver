5554a18cc616c631f235353dac9ceeed3cb98274
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-07 17:31:24 +0000

    Made a change in the implementation of the destructor in the Tile header

--- src/Tile.h
+++ src/Tile.h
@@ -4,7 +4,7 @@
 class Tile{
     public:
         Tile(int tileWd, int tileHt); //Constructor
-        Tile(); //Destructor  
+        ~Tile(); //Destructor  
 
         unsigned char getPixel(int x, int y) const; //get pixel value
         void setPixel(int x, int y, unsigned char val); //set pixel value
