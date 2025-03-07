24e09ff0fda003ad81ccfea8d983426c88055041
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-07 16:54:18 +0000

    Implemented the getPixel and setPixel methods in Tile header

--- src/Tile.h
+++ src/Tile.h
@@ -5,4 +5,7 @@ class Tile{
     public:
         Tile(int tileWd, int tileHt); //Constructor
         Tile(); //Destructor  
+
+        unsigned char getPixel(int x, int y) const; //get pixel value
+        void setPixel(int x, int y, unsigned char val); //set pixel value
 };
