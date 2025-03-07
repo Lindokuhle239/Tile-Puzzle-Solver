37983a110e4f1ff31d0df14d9d9fdb39aa08b7de
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-07 18:17:41 +0000

    Added the private attributes of the TileManger header

--- src/TileManager.h
+++ src/TileManager.h
@@ -11,4 +11,11 @@ class TileManager{
 
         void generateRandMove(); //generate a random valid move
         void writeBoardState(const std::string& fileName) const; //write the board state to a pgm file
+
+    private:
+        std::vector<std::vector<Tile>> grid; //grid of tiles
+        int emptyRow, emptyCol; //position of the empty file
+        int gridSize; //size of the grid
+        int imageWd, imageHt; //dimensions of the input image
+        int tileWd, tileHt; //dimensions of each tile
 }
