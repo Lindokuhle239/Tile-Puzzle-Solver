6017d27829752556d2d06decc9a585181bf24ab8
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-10 20:29:28 +0000

    Updated the main function in tileManagerTest, to fix the randomization of tiles to move into an empty space in the game

---
+++ src/tileManagerTest.cpp
@@ -0,0 +1,19 @@
+#include "TileManager.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+int main(){
+    //seed the random num generator
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
+    TileManager manager(3, "test_files/mona512.pgm");
+
+    for (int i = 1; i <= 5; ++i){
+        manager.generateRandMove();
+        manager.writeBoardState("output-" + std::to_string(i) + ".pgm");
+    }
+
+    std::cout << "Generated 5 random moves and write output images" << std::endl;
+    return 0;
+}
