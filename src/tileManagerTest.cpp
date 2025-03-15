7ed3865ead293c7a72a32a7e122e693f8508ddb9
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-03-15 07:06:00 +0000

    Send up the command line arguments for when user runs the program

--- src/tileManagerTest.cpp
+++ src/tileManagerTest.cpp
@@ -2,16 +2,44 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <string>
 
-int main(){
+int main(int argc, char* argv[]){
+    //default vals
+    std::string inputImageName = "input.pgm";
+    std::string outputBaseName = "output";
+    std::string summaryImageName = "";
+    int gridSize = 3;
+    int numMoves = 5;
+
+    //parse command-line arguments
+    for (int i = 1; i < argc; ++i){
+        std::string arg = argv[i];
+        if ((arg == "-s") && (i + 1 < argc)){
+            gridSize = std::stoi(argv[++i]);
+        }
+        else if ((arg == "-o") && (i + 1 < argc)){
+            outputBaseName = argv[++i];
+        }
+        else if ((arg == "-n") && (i + 1 < argc)){
+            numMoves = std::stoi(argv[++i]);
+        }
+        else if ((arg == "-x") && (i + 1 < argc)){
+            summaryImageName = argv[++i];
+        }
+        else{
+            inputImageName = arg;
+        }
+    }
+
     //seed the random num generator
     std::srand(static_cast<unsigned int>(std::time(0)));
 
-    TileManager manager(3, "test_files/mona512.pgm");
+    TileManager manager(5, "test_files/mona512.pgm");
 
     manager.writeBoardState("output-0.pgm");
 
-    for (int i = 1; i <= 5; ++i){
+    for (int i = 1; i <= 10; ++i){
         manager.generateRandMove();
         manager.writeBoardState("output-" + std::to_string(i) + ".pgm");
     }
